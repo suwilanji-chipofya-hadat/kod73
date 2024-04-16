ec4d95dfbc475bb49affb23fba9fffb461a9052d
suwilanji-chipofya-hadat <suwilanjichipofya@outlook.com>
2024-04-16 15:41:14 +0200

    Fix vercel handler error

--- api/index.py
+++ api/index.py
@@ -12,7 +12,7 @@ if not TOKEN:
 if not SITE_URL:
     raise Exception("Site Url Not Found")
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+app = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
@@ -25,7 +25,7 @@ def echo_message(message):
     bot.reply_to(message, message.text)
 
 
-@server.route('/' + TOKEN, methods=['POST'])
+@app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
@@ -33,7 +33,7 @@ def getMessage():
     return "!", 200
 
 
-@server.route("/")
+@app.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url=SITE_URL + TOKEN)
@@ -41,4 +41,4 @@ def webhook():
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
