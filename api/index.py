d8cb26fe5f67f4667ca9a78557a9e9829655716d
suwilanji-chipofya-hadat <suwilanjichipofya@outlook.com>
2024-04-16 15:37:52 +0200

    Initial configuration

--- api/index.py
+++ api/index.py
@@ -1,11 +1,44 @@
-from flask import Flask
+import os
+from flask import Flask, request
+import telebot
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+load_dotenv()
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+TOKEN = os.environ.get("BOT_TOKEN")
+SITE_URL = os.environ.get("SITE_URL")
+if not TOKEN:
+    raise Exception("Token Not Found")
+if not SITE_URL:
+    raise Exception("Site Url Not Found")
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
-@app.route('/about')
-def about():
-    return 'About'
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=SITE_URL + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
